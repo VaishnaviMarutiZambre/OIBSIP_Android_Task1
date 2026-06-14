993a5d7bac2e2268a02e0f3024b19f4ac71f74c8
VaishnaviMarutiZambre <zambrevaishanvi@gmail.com>
2026-06-14 12:20:36 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,18 @@
+public class MainActivity {
+
+    public double cmToMeter(double cm) {
+        return cm / 100;
+    }
+
+    public double meterToCm(double meter) {
+        return meter * 100;
+    }
+
+    public double gramToKg(double gram) {
+        return gram / 1000;
+    }
+
+    public double kgToGram(double kg) {
+        return kg * 1000;
+    }
+}
